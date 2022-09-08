850990192a3cc9c95e0c853a6357267aa27616c3
Waldomiro Freitas Barbosa <miro@wfbconsultoria.com.br>
2022-09-08 18:25:35 -0300

    CONFIRMADO BASH

---
+++ SIASG/SIASG_BUILD.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo dotnet build /home/bih_datasus/SIASG/SIASG/SIASG_DOWNLOAD/SIASG_DOWNLOAD.vbproj --os linux
+sudo dotnet build /home/bih_datasus/SIASG/SIASG/SIASG_IMPORT/SIASG_IMPORT.vbproj --os linux
+sudo dotnet run /home/bih_datasus/SIASG/SIASG/SIASG_DOWNLOAD/bin/Debug/net6.0/linux-x64/SIASG_DOWNLOAD
+sudo dotnet run /home/bih_datasus/SIASG/SIASG/SIASG_IMPORT/bin/Debug/net6.0/linux-x64/SIASG_IMPORT
