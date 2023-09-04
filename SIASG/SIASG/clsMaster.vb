d8c7d1ac04d16ca83c93b865a43ac85fa37c8f74
Waldomiro Freitas Barbosa <miro@wfbconsultoria.com.br>
2023-09-04 19:14:33 -0300

    confirmado

--- SIASG/SIASG/clsMaster.vb
+++ SIASG/SIASG/clsMaster.vb
@@ -1,10 +1,19 @@
-﻿Public Class clsMaster
+﻿Imports System.Net
+Imports System.IO
+Imports System.Net.Http
+
+Public Class clsMaster
     Implements IDisposable
+    Private disposedValue As Boolean
 
     Private cnn As New Npgsql.NpgsqlConnection 'Conexão Postgres
     Private Const cnnStr As String = "Server=paris.datasus.net.br;Port=5432;UserId=bih_datasus;Password=Mepm2412;Database=bih_datasus"
-    Private Const schema As String = "SIASG"
-    Private disposedValue As Boolean
+    Public Const schema As String = "SIASG"
+
+    'Public Const pasta_downloads As String = "/home/bih_datasus/SIASG/SIASG/DOWNLOADS/"
+    Public Const pasta_downloads As String = "D:\DOWNLOADS"
+    Public Const MyURL As String = "http://compras.dados.gov.br/materiais/v1/grupos.csv"
+    Private Shared ReadOnly HttpClient As HttpClient = New HttpClient()
 
     Public Function ConectaBanco() As Boolean
         Try
@@ -64,6 +73,19 @@
         End Try
         ConverteSQL_PSQL = SQL
     End Function
+    Public Function Download(remoteUri As String, filename As String) As Boolean
+        Try
+            Using myWebClient As New WebClient()
+                myWebClient.DownloadFile(remoteUri, filename)
+            End Using
+            Download = True
+        Catch e As Exception
+            Download = False
+        Finally
+
+        End Try
+    End Function
+
     Protected Overridable Sub Dispose(disposing As Boolean)
         If Not disposedValue Then
             If disposing Then
@@ -82,7 +104,6 @@
     '     Dispose(disposing:=False)
     '     MyBase.Finalize()
     ' End Sub
-
     Public Sub Dispose() Implements IDisposable.Dispose
         ' Não altere este código. Coloque o código de limpeza no método 'Dispose(disposing As Boolean)'
         Dispose(disposing:=True)
